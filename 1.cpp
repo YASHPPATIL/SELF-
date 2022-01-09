445685af5828bc990e3305869786d43201cdf631
YashPPatil <ypatil_b20@ee.vjti.ac.in>
2022-01-09 12:48:16 +0530

    modified commit

---
+++ 1.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+using namespace std;
+int main() {
+     //float x= 489.8;
+     //float &y =x ;
+     //cout<<x<<endl;
+     //cout<<y<<endl;
+int c=45; float b=45.8;
+
+cout<<float(c)<<endl;
+cout<<int(b)<<endl;
+
+
+
+
+     return 0;
+}
---
+++ note1.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+using namespace std;
+int main() {
+     //float x= 489.8;
+     //float &y =x ;
+     //cout<<x<<endl;
+     //cout<<y<<endl;
+int a=45; float b=45.7;
+
+cout<<float(a)<<endl;
+cout<<int(b)<<endl;
+
+
+
+
+     return 0;
+}
